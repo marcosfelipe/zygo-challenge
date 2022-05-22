50acbea5dc76886dfda84944d53974109b8e6bee
marcos <marcos.dasilva@castgroup.com.br>
2022-05-22 20:08:11 -0300

    add feature for book list

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'home', type: :feature do
+  setup do
+    Book.create!(title: 'Test', description: 'test', picture: 'test', author: 'test')
+  end
+  
   scenario('home page') do
     visit(root_path)
     expect(page).to have_content('Home')
+  end
+  
+  scenario 'anonymous user visualizes the book list' do
+    visit(root_path)
+    expect(page).to have_content('Title: Test')
   end
 end
