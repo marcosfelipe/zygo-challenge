2928c3a9f7f561771b5d479e33d33ae3ad1bc8d3
marcos <marcos.dasilva@castgroup.com.br>
2022-05-22 22:29:53 -0300

    helper to create the input texts with field errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def input_errors_for(form, field_name)
+    return if form.object.errors[field_name].blank?
+    content_tag(:span, class: 'helper-text red-text') do
+      form.object.errors[field_name].join(',')
+    end
+  end
+  
+  def input_text(form, field_name)
+    content_tag(:div, class: 'input-field') do
+      concat form.text_field(field_name)
+      concat form.label(field_name.capitalize, for: "#{form.object.class.to_s.downcase}_field_name")
+      concat input_errors_for(form, field_name)
+    end
+  end
 end
