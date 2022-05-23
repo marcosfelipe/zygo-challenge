8d43c5dd485d6fb4ffaade93061ec2235163ca75
marcos <marcos.dasilva@castgroup.com.br>
2022-05-23 14:43:58 -0300

    test create a book

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'home', type: :feature do
   setup do
-    Book.create!(title: 'Test', description: 'test', picture: 'test', author: 'test')
+    Book.create!(title: 'Test', description: 'test', 
+                 picture: fixture_file_upload('dummy.png'), 
+                 author: 'test')
   end
   
   scenario('home page') do
@@ -26,4 +28,24 @@ RSpec.describe 'home', type: :feature do
     click_on class: 'btn-floating'
     expect(page).to have_content('New Book')
   end
+  
+  scenario 'logged user can create a book' do
+    user = FactoryBot.create(:user)
+    visit '/sign_in'
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: user.password
+    click_on 'Log in'
+    visit(root_path)
+    click_on class: 'btn-floating'
+    expect(page).to have_content('New Book')
+    fill_in :book_description, with: 'test'
+    fill_in :book_author, with: 'test'
+    fill_in :book_title, with: 'test'
+    attach_file 'book_picture', Rails.root.join('spec', 'fixtures', 'files', 'dummy.png')
+    click_on 'Save'
+    expect(page).to have_content('Book saved!')
+  end
+  
+  # TODO: add test for create
+  # TODO: add test for edit/update
 end
