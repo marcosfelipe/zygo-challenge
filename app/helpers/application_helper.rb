7c88926d7c2cdafaf7a76b4f4c935abbffff5b53
marcos <marcos.dasilva@castgroup.com.br>
2022-05-22 23:58:16 -0300

    fix floating label

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   def input_text(form, field_name)
     content_tag(:div, class: 'input-field') do
       concat form.text_field(field_name)
-      concat form.label(field_name.capitalize, for: "#{form.object.class.to_s.downcase}_field_name")
+      concat form.label(field_name.capitalize, for: "#{form.object.class.to_s.downcase}_#{field_name}")
       concat input_errors_for(form, field_name)
     end
   end
