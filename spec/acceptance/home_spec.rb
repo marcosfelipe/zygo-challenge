f0c7eeee08b3d1669b77d35c65b05a05a463c0b9
marcos <marcos.dasilva@castgroup.com.br>
2022-05-24 17:40:34 -0300

    refactor: dry login steps

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -17,35 +17,34 @@ RSpec.describe 'home', type: :feature do
     expect(page).to have_content('Test')
   end
   
-  scenario 'logged user can open the new book form' do
-    user = FactoryBot.create(:user)
-    visit '/sign_in'
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: user.password
-    click_on 'Log in'
-    visit(root_path)
-    expect(page).to have_content('sign out')
-    click_on class: 'btn-floating'
-    expect(page).to have_content('New Book')
+  context 'logged user' do
+    before do
+      user = FactoryBot.create(:user)
+      visit '/sign_in'
+      fill_in :user_email, with: user.email
+      fill_in :user_password, with: user.password
+      click_on 'Log in'
+      expect(page).to have_current_path(root_path)
+    end
+
+    scenario 'logged user can open the new book form' do
+      expect(page).to have_content('sign out')
+      click_on class: 'btn-floating'
+      expect(page).to have_content('New Book')
+    end
+
+    scenario 'logged user can create a book' do
+      click_on class: 'btn-floating'
+      expect(page).to have_content('New Book')
+      fill_in :book_description, with: 'test'
+      fill_in :book_author, with: 'test'
+      fill_in :book_title, with: 'test'
+      attach_file 'book[picture]', Rails.root.join('spec', 'fixtures', 'files', 'dummy.png'), make_visible: true
+      click_on 'Save'
+      expect(page).to have_content('Book saved!')
+    end
   end
   
-  scenario 'logged user can create a book' do
-    user = FactoryBot.create(:user)
-    visit '/sign_in'
-    fill_in :user_email, with: user.email
-    fill_in :user_password, with: user.password
-    click_on 'Log in'
-    visit(root_path)
-    click_on class: 'btn-floating'
-    expect(page).to have_content('New Book')
-    fill_in :book_description, with: 'test'
-    fill_in :book_author, with: 'test'
-    fill_in :book_title, with: 'test'
-    attach_file 'book[picture]', Rails.root.join('spec', 'fixtures', 'files', 'dummy.png'), make_visible: true
-    click_on 'Save'
-    expect(page).to have_content('Book saved!')
-  end
-  
-  # TODO: add test for create
   # TODO: add test for edit/update
+  # TODO: favorite books
 end
