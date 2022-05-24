a7d094e90268421a092390222c5c0fd903f36726
marcos <marcos.dasilva@castgroup.com.br>
2022-05-23 21:45:17 -0300

    add a select to sort the list

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select')
+  var instances = M.FormSelect.init(elems, {})
+});
