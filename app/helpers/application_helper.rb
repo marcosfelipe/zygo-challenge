ee7561c9cca625cc4c8cc7a5531158254ee58067
marcos <marcos.dasilva@castgroup.com.br>
2022-05-23 22:20:22 -0300

    fix method overload

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   
   def input_text(form, field_name)
     content_tag(:div, class: 'input-field') do
-      concat form.text_field(field_name, options)
+      concat form.text_field(field_name)
       concat form.label(field_name.capitalize, for: "#{form.object.class.to_s.downcase}_#{field_name}")
       concat input_errors_for(form, field_name)
     end
