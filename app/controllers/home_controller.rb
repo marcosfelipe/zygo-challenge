c6f3b9d9daead5ef12189a29cd20d19842b96014
marcos <marcos.dasilva@castgroup.com.br>
2022-05-23 21:22:49 -0300

    refactor reduce the controller call complexity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   
   def index
-    @books = Book.search(params[:search]).paginate(page: params[:page], per_page: 1)
+    @books = Book.paginate_search(params)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ApplicationRecord
   validates_presence_of :title, :author, :description, :picture
 
   mount_uploader :picture, BookPictureUploader
+  
+  scope :paginate_search, -> (params = {}) { search(params[:search]).paginate(page: params[:page]) }
 end
