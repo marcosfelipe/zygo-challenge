e32cb173862256f66f6badaaa14a2dbca1a499a1
marcos <marcos.dasilva@castgroup.com.br>
2022-05-24 17:57:10 -0300

    test the login page

--- spec/acceptance/login_spec.rb
+++ spec/acceptance/login_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe 'login', type: :feature do
     visit('/sign_in')
     expect(page).to have_content('Log in')
   end
+  
+  scenario('valid credentials') do
+    user = FactoryBot.create(:user)
+    visit('/sign_in')
+    fill_in :user_email, with: user.email
+    fill_in :user_password, with: user.password
+    click_on 'Log in'
+    expect(page).to have_current_path(root_path)
+    expect(page).to have_content('Signed in successfully')
+  end
+  
+  scenario('invalid credentials') do
+    visit('/sign_in')
+    fill_in :user_email, with: 'not@user.com'
+    fill_in :user_password, with: 'notuser'
+    click_on 'Log in'
+    expect(page).to have_content('Invalid Email or password')
+  end
 end
